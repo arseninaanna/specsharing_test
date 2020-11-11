17bb7ed97411e87029e67c94030b59a9cc0141cc
Anna <arseninaanna@mail.ru>
2020-11-11 12:43:05 +0300

    fixed bugs

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import User, Wallet, Currency
+from .models import User, Wallet, CurrencyMap
 
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Wallet)
-admin.site.register(Currency)
+admin.site.register(CurrencyMap)
